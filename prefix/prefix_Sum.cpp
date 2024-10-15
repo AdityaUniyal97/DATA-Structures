fcf4f2ce6a10ad8540a6660ca9e46b982324c3c3
AdityaUniyal97 <160417678+AdityaUniyal97@users.noreply.github.com>
2024-10-15 20:49:09 +0530

    .

--- prefix/prefix_Sum.cpp
+++ prefix/prefix_Sum.cpp
@@ -1,7 +1,30 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
 
-int main() {
-  cout << "Hello World!";
-  return 0;
+int main()
+{
+    int arr[] = {10, 4, 16, 20, 9, 1};
+    int n = 6;
+    int pre[n];
+    pre[0] = arr[0];
+    for(int i = 1; i < n ; i++)
+    {
+        pre[i] = pre[i - 1] + arr[i];
+    }
+    int q ;
+    cin >> q;
+    while(q--){
+        int L , R;
+        cin >> L >> R;
+        int sum = 0;
+        if(L == 0)
+        {
+            sum = pre[R];
+        }
+        else{
+            sum = pre[R] - pre[L - 1];
+        }
+      cout << sum << endl;  
+    }
+    return 0;
 }
